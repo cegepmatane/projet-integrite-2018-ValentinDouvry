2315f79eaae6e325294e6eb4f0bca11229fcb13e
DouvryValentin <valentin.jabba@gmail.com>
2018-09-05 20:48:59 -0400

    modele stationnement

---
+++ src/modele/Stationnement.java
@@ -0,0 +1,46 @@
+package modele;
+
+public class Stationnement 
+{
+	protected String nom;
+	protected String nombrePlace;
+	protected String ville;
+	protected String nombreEtage;
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNombrePlace() {
+		return nombrePlace;
+	}
+	
+	public void setNombrePlace(String nombrePlace) {
+		this.nombrePlace = nombrePlace;
+	}
+	
+	public String getVille() {
+		return ville;
+	}
+	
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	
+	public String getNombreEtage() {
+		return nombreEtage;
+	}
+	
+	public void setNombreEtage(String nombreEtage) {
+		this.nombreEtage = nombreEtage;
+	}
+	
+	
+	
+	
+}
