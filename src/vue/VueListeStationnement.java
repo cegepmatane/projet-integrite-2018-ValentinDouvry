6c3ca93106c89924f0c060f88297d284779fa8c8
DouvryValentin <valentin.jabba@gmail.com>
2018-09-05 21:30:30 -0400

    creation liste de Stationnement + fonction affichage vue liste de Stationnement

--- src/vue/VueListeStationnement.java
+++ src/vue/VueListeStationnement.java
@@ -1,23 +1,45 @@
 package vue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import modele.Stationnement;
 
 public class VueListeStationnement extends Application
 {
+	protected GridPane grilleStationnements;
 
 	@Override
 	public void start(Stage stade) throws Exception 
 	{
 		Pane panneau = new Pane();
-		GridPane grilleStationnements = new GridPane();
+		grilleStationnements = new GridPane();
 		
 		panneau.getChildren().add(grilleStationnements);
 		stade.setScene(new Scene(panneau,500,500));
 		stade.show();
+		
+		/*TEST*/
+		List listeStationnementsTest = new ArrayList<Stationnement>();
+		listeStationnementsTest.add(new Stationnement("Parking de la neige", "150 places", "Paris", "3"));
+		listeStationnementsTest.add(new Stationnement("Parking de la reine", "400 places", "Londre", "4"));
+		listeStationnementsTest.add(new Stationnement("Parking de la courtoisie", "12 places", "OUI", "1"));
+		listeStationnementsTest.add(new Stationnement("Parking de Vincent", "900 places", "Strasbourg", "10"));
+		
+		
+		this.afficherListeStationnement(listeStationnementsTest);
+		
+		
+	}
+	
+	public void afficherListeStationnement(List<Stationnement> listeStationnements)
+	{
+		
 	}
 	
 }
