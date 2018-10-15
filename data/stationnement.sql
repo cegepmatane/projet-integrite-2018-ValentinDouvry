3b2d0a8d6a6937864a37b9c34f92fc1330e68854
DouvryValentin <valentin.jabba@gmail.com>
2018-10-15 14:14:24 -0400

    Changement fichier SQL sans insert

--- data/stationnement.sql
+++ data/stationnement.sql
@@ -3,13 +3,10 @@
 --
 
 -- Dumped from database version 9.5.14
--- Dumped by pg_dump version 10.4
-
--- Started on 2018-10-04 06:31:24
+-- Dumped by pg_dump version 9.5.14
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -18,7 +15,26 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 1 (class 3079 OID 12355)
+-- Name: Stationnement; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE "Stationnement" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'French_Canada.1252' LC_CTYPE = 'French_Canada.1252';
+
+
+ALTER DATABASE "Stationnement" OWNER TO postgres;
+
+\connect "Stationnement"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -26,8 +42,6 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2123 (class 0 OID 0)
--- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -39,7 +53,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 182 (class 1259 OID 24629)
 -- Name: stationnement; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -55,7 +68,6 @@ CREATE TABLE public.stationnement (
 ALTER TABLE public.stationnement OWNER TO postgres;
 
 --
--- TOC entry 181 (class 1259 OID 24627)
 -- Name: stationnement_id_stationnement_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -70,8 +82,6 @@ CREATE SEQUENCE public.stationnement_id_stationnement_seq
 ALTER TABLE public.stationnement_id_stationnement_seq OWNER TO postgres;
 
 --
--- TOC entry 2124 (class 0 OID 0)
--- Dependencies: 181
 -- Name: stationnement_id_stationnement_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -79,7 +89,6 @@ ALTER SEQUENCE public.stationnement_id_stationnement_seq OWNED BY public.station
 
 
 --
--- TOC entry 184 (class 1259 OID 24640)
 -- Name: voiture; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -96,7 +105,6 @@ CREATE TABLE public.voiture (
 ALTER TABLE public.voiture OWNER TO postgres;
 
 --
--- TOC entry 183 (class 1259 OID 24638)
 -- Name: voiture_id_voiture_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -111,8 +119,6 @@ CREATE SEQUENCE public.voiture_id_voiture_seq
 ALTER TABLE public.voiture_id_voiture_seq OWNER TO postgres;
 
 --
--- TOC entry 2125 (class 0 OID 0)
--- Dependencies: 183
 -- Name: voiture_id_voiture_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -120,45 +126,27 @@ ALTER SEQUENCE public.voiture_id_voiture_seq OWNED BY public.voiture.id_voiture;
 
 
 --
--- TOC entry 1989 (class 2604 OID 24632)
--- Name: stationnement id_stationnement; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: id_stationnement; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.stationnement ALTER COLUMN id_stationnement SET DEFAULT nextval('public.stationnement_id_stationnement_seq'::regclass);
 
 
 --
--- TOC entry 1990 (class 2604 OID 24643)
--- Name: voiture id_voiture; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: id_voiture; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.voiture ALTER COLUMN id_voiture SET DEFAULT nextval('public.voiture_id_voiture_seq'::regclass);
 
 
 --
--- TOC entry 2112 (class 0 OID 24629)
--- Dependencies: 182
 -- Data for Name: stationnement; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.stationnement (id_stationnement, nom, nombre_place, nombre_etage, ville) FROM stdin;
-1	Roger Parking Test Modification	101	155	Rimouski
-\.
+INSERT INTO public.stationnement VALUES (1, 'Roger Parking Test Modification', '101', '155', 'Rimouski');
 
 
 --
--- TOC entry 2114 (class 0 OID 24640)
--- Dependencies: 184
--- Data for Name: voiture; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.voiture (id_voiture, marque, modele, puissance, couleur, id_stationnement) FROM stdin;
-\.
-
-
---
--- TOC entry 2126 (class 0 OID 0)
--- Dependencies: 181
 -- Name: stationnement_id_stationnement_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -166,8 +154,12 @@ SELECT pg_catalog.setval('public.stationnement_id_stationnement_seq', 2, true);
 
 
 --
--- TOC entry 2127 (class 0 OID 0)
--- Dependencies: 183
+-- Data for Name: voiture; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
 -- Name: voiture_id_voiture_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -175,8 +167,7 @@ SELECT pg_catalog.setval('public.voiture_id_voiture_seq', 1, true);
 
 
 --
--- TOC entry 1992 (class 2606 OID 24637)
--- Name: stationnement stationnement_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: stationnement_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.stationnement
@@ -184,8 +175,7 @@ ALTER TABLE ONLY public.stationnement
 
 
 --
--- TOC entry 1995 (class 2606 OID 24648)
--- Name: voiture voiture_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: voiture_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.voiture
@@ -193,7 +183,6 @@ ALTER TABLE ONLY public.voiture
 
 
 --
--- TOC entry 1993 (class 1259 OID 24660)
 -- Name: fki_voiture_idstationnement_fkey; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -201,8 +190,7 @@ CREATE INDEX fki_voiture_idstationnement_fkey ON public.voiture USING btree (id_
 
 
 --
--- TOC entry 1996 (class 2606 OID 24661)
--- Name: voiture voiture_idstationnement_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: voiture_idstationnement_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.voiture
@@ -210,8 +198,6 @@ ALTER TABLE ONLY public.voiture
 
 
 --
--- TOC entry 2122 (class 0 OID 0)
--- Dependencies: 6
 -- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
 --
 
@@ -220,8 +206,6 @@ REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
-
--- Completed on 2018-10-04 06:31:25
 
 --
 -- PostgreSQL database dump complete
